802424ac5cc9e136ce34cc1ad5cae7bdef983d37
Chandrbhan Singh <chandrbhankumar8@gmail.com>
2024-07-12 16:07:21 +0530

    AppTest.java 15 line modification

--- GITMavenProj01/src/test/java/com/nt/test/AppTest.java
+++ GITMavenProj01/src/test/java/com/nt/test/AppTest.java
@@ -13,8 +13,8 @@ public class AppTest {
 	@Test
 	public void testWithPositive() {
 		Arithmetic ar=new Arithmetic();
-		int expected=40;
-		int actual=ar.sum(20, 20);
+		int expected=60;
+		int actual=ar.sum(40, 20);
 		assertEquals(expected, actual);
 	}
 	
