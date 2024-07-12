519c91e3463c6c7842e94ebbf38cb995e33d4c09
Chandrbhan Singh <chandrbhankumar8@gmail.com>
2024-07-12 15:53:40 +0530

    adding more tests

--- GITMavenProj01/src/test/java/com/nt/test/AppTest.java
+++ GITMavenProj01/src/test/java/com/nt/test/AppTest.java
@@ -17,5 +17,20 @@ public class AppTest {
 		int actual=ar.sum(20, 20);
 		assertEquals(expected, actual);
 	}
+	
+	@Test
+	public void testWithNegative() {
+		Arithmetic ar=new Arithmetic();
+		int expected=-40;
+		int actual=ar.sum(-20, -20);
+		assertEquals(expected, actual);
+	}
+	@Test
+	public void testWithZero() {
+		Arithmetic ar=new Arithmetic();
+		int expected=0;
+		int actual=ar.sum(0, 0);
+		assertEquals(expected, actual);
+	}
 
 }
